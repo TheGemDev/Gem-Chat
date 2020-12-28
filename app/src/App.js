98c1df408381e583ef203ee0d3559d4ef3f75fef
The Gem Dev <53819287+the-gem-dev@users.noreply.github.com>
2020-12-28 14:33:52 +0100

    update

---
+++ app/src/App.js
@@ -0,0 +1,130 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCfrS9gSP-xPdNFhA-1LHmMbFdRevlzxLs",
+  authDomain: "gem-chat-app.firebaseapp.com",
+  databaseURL: "https://gem-chat-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gem-chat-app",
+  storageBucket: "gem-chat-app.appspot.com",
+  messagingSenderId: "378782366095",
+  appId: "1:378782366095:web:77ab77050f738839a9b3fa",
+  measurementId: "G-ML6QFR1K72"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Gem Chat :'(</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Chat here Okay!!!!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(100);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage} style={{position: "sticky"}}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img alt={'img'} src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
---
+++ app/src/services/firebase/firebase.js
@@ -0,0 +1,17 @@
+import { firebase } from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfrS9gSP-xPdNFhA-1LHmMbFdRevlzxLs",
+    authDomain: "gem-chat-app.firebaseapp.com",
+    databaseURL: "https://gem-chat-app-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gem-chat-app",
+    storageBucket: "gem-chat-app.appspot.com",
+    messagingSenderId: "378782366095",
+    appId: "1:378782366095:web:77ab77050f738839a9b3fa",
+    measurementId: "G-ML6QFR1K72"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const database = firebase.database();
+
